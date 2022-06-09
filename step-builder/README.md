84990015a8b7316d8847774c6f7e49cbc66a7f2d
Sebastien Vermeille <sebastien.vermeille@geo-satis.com>
2022-06-09 17:00:33 +0200

    Provide an example of step-builder pattern implementation This way I can use it as a reference for my next blog article

---
+++ step-builder/README.md
@@ -0,0 +1,10 @@
+Step Builder - Design pattern
+==========================================
+
+## Pro
+* Provide fluent assistance to developer (each steps of the construct of an object is guided)
+* No longer forget mandatory properties (compile time detection)
+
+## Cons
+* More complicated to write and maintain by hand. It is highly suggested to use a code generator for that purpose. (i.e.: 
+  [stepbuilder-codegen](https://plugins.jetbrains.com/plugin/16792-stepbuilder-codegen))
---
+++ step-builder/src/test/java/ch/svermeille/design/pattern/step/builder/EmailTest.java
@@ -0,0 +1,35 @@
+package ch.svermeille.design.pattern.step.builder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Sebastien Vermeille
+ */
+class EmailTest {
+
+  @Test
+  void anInstantiatedEmailObjectShouldProvideTheGivenPropertiesBackGracefully(){
+
+    // GIVEN
+    final var from = "no-reply@great-company.com";
+    final var to = "john.doe@gmail.com";
+    final var title = "Newsletter 42";
+    final var content = "Hi John, ...";
+
+    // WHEN
+    final var email = Email.builder()
+        .withFrom(from)
+        .withTo(to)
+        .withTitle(title)
+        .withContent(content)
+        .build();
+
+    // THEN
+    assertThat(email.getFrom()).isEqualTo(from);
+    assertThat(email.getTo()).isEqualTo(to);
+    assertThat(email.getTitle()).isEqualTo(title);
+    assertThat(email.getContent()).isEqualTo(content);
+  }
+}
